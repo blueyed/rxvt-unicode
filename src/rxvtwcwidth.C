1401c82959947fd93a7db6e7cb663161fe98cb6b
Daniel Hahler <git@thequod.de>
2016-11-05 02:45:48 +0100

    add src/rxvtwcwidth.C

---
+++ src/rxvtwcwidth.C
@@ -0,0 +1,181 @@
+/*
+ * Shared object to override wcwidth/wcswidth, which asks rxvt-unicode for the
+ * width of a char.
+ *
+ * TODO:
+ * - is it possible from rxvt-unicode to detect that the client does not use
+ *   its rxvtwcwidth.so?  In that case rxvt-unicode might fall back to the
+ *   system's, too - or rather just be more careful.
+ *   This happens e.g. when attaching to a tmux session, which was started
+ *   without the wrapper.
+ *
+ * XXX: this is quite a mess still.
+ *  - would it be possible to have a terminal escape char interface for this?!
+ */
+
+#include "../config.h"          /* NECESSARY */
+#include "rxvt.h"               /* NECESSARY */
+#include <sys/socket.h>
+#include <sys/un.h>
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <fcntl.h>
+
+#include "rxvtwcwidth.h"
+
+MapType wcwidth_cache;
+
+typedef int (*orig_wcwidth_f_type)(wchar_t c);
+int
+orig_wcwidth(wchar_t c)
+{
+    orig_wcwidth_f_type wcwidth;
+    wcwidth = (orig_wcwidth_f_type)dlsym(RTLD_NEXT, "wcwidth");
+    return wcwidth(c);
+}
+
+
+int _wcwidth(wchar_t c)
+{
+  const char *wcwidth_socket_name = getenv("RXVT_WCWIDTH_SOCKET");
+  if (!wcwidth_socket_name) {
+#ifdef DEBUG_WCWIDTH_CLIENT
+    // TODO: use global to display errors only once.
+    fprintf(stderr, "RXVT_WCWIDTH_SOCKET is not set.\n");
+#endif
+    return orig_wcwidth(c);
+  }
+
+  /* Cached? */
+  MapType::iterator it;
+  it = wcwidth_cache.find(c);
+  if(it != wcwidth_cache.end()) {
+    return it->second;
+  }
+
+#ifdef DEBUG_WCWIDTH_CLIENT
+  fprintf(stderr, "_wcwidth: connecting to socket: %s\n", wcwidth_socket_name);
+#endif
+  int wcwidth_socket_fd = socket(AF_UNIX, SOCK_SEQPACKET, 0);
+  if (wcwidth_socket_fd == -1) {
+    perror("socket");
+    return orig_wcwidth(c);
+  }
+
+  /* Bind socket to socket name. */
+  struct sockaddr_un name;
+  name.sun_family = AF_UNIX;
+  strcpy(name.sun_path, wcwidth_socket_name);
+  int len = strlen(name.sun_path) + sizeof(name.sun_family);
+
+  // Write should be non-blocking in case the parent is gone (tmux).
+  int orig_flags;
+  orig_flags = fcntl(wcwidth_socket_fd, F_GETFL);
+  fcntl(wcwidth_socket_fd, F_SETFL, orig_flags | O_NONBLOCK);
+
+  if (-1 == connect(wcwidth_socket_fd, (struct sockaddr *)&name, len)) {
+    fprintf(stderr, "Could not connect to socket %s: %s (%i)\n",
+        wcwidth_socket_name, strerror(errno), errno);
+    if (errno == EINPROGRESS) {
+      struct timeval tv;
+      fd_set myset;
+      fprintf(stderr, "EINPROGRESS in connect() - selecting\n");
+      do {
+        tv.tv_sec = 1;
+        tv.tv_usec = 0;
+        FD_ZERO(&myset);
+        FD_SET(wcwidth_socket_fd, &myset);
+        int ret = select(wcwidth_socket_fd+1, NULL, &myset, NULL, &tv);
+        if (ret < 0 && errno != EINTR) {
+          perror("Error connecting");
+          return orig_wcwidth(c);
+        }
+        else if (ret > 0) {
+          // Socket selected for write
+          socklen_t lon;
+          lon = sizeof(int);
+          int valopt;
+          if (getsockopt(wcwidth_socket_fd, SOL_SOCKET, SO_ERROR, (void*)(&valopt), &lon) < 0) {
+            perror("getsockopt");
+            return orig_wcwidth(c);
+          }
+          // Check the value returned...
+          if (valopt) {
+            perror("Error in delayed connection()");
+            return orig_wcwidth(c);
+          }
+          break;
+        }
+        else {
+          perror("Timeout in select() - Cancelling!");
+          return orig_wcwidth(c);
+        }
+      } while (1);
+    }
+    perror("connect");
+    return orig_wcwidth(c);
+  }
+  fcntl(wcwidth_socket_fd, F_SETFL, orig_flags);
+
+
+  struct timeval timeout;
+  timeout.tv_sec = 1;
+  timeout.tv_usec = 0;
+
+  if (setsockopt (wcwidth_socket_fd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,
+        sizeof(timeout)) < 0)
+    perror("setsockopt failed\n");
+
+  if (setsockopt (wcwidth_socket_fd, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout,
+        sizeof(timeout)) < 0)
+    perror("setsockopt failed\n");
+
+
+#ifdef DEBUG_WCWIDTH_CLIENT
+  fprintf(stderr, "_wcwidth: sending query: %lc\n", c);
+#endif
+  int ret = write(wcwidth_socket_fd, &c, sizeof(wchar_t));
+  if (ret == -1) {
+    perror("write");
+    return orig_wcwidth(c);
+  }
+
+#ifdef DEBUG_WCWIDTH_CLIENT
+  fprintf(stderr, "_wcwidth: reading answer\n");
+#endif
+  int width;
+    ret = read(wcwidth_socket_fd, &width, sizeof(int));
+    if (ret == -1) {
+        perror("read");
+        return orig_wcwidth(c);
+    }
+#ifdef DEBUG_WCWIDTH_CLIENT
+  fprintf(stderr, "_wcwidth: read: %i\n", width);
+#endif
+  wcwidth_cache[c] = width;
+  close(wcwidth_socket_fd);
+  return width;
+}
+
+
+int wcwidth(wchar_t c) {
+  orig_wcwidth_f_type wcwidth = _wcwidth;
+  return WCWIDTH(c);
+}
+
+
+/* This seems to be required, since it will not always use (the injected)
+ * wcwidth?!
+ */
+int wcswidth(const wchar_t *pwcs, size_t n)
+{
+  int w, width = 0;
+
+  for (;*pwcs && n-- > 0; pwcs++)
+    if ((w = wcwidth(*pwcs)) < 0)
+      return -1;
+    else
+      width += w;
+
+  return width;
+}
---
+++ src/rxvtwcwidth.h
@@ -0,0 +1,3 @@
+#include <map>
+typedef std::map<const wchar_t, int> MapType;
+// int wcwidth(wchar_t c);
